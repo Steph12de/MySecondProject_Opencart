6cf1d77076a82e8865a4d8666c07a070742d4b97
Steph12de <pokaarmelle12@yahoo.fr>
2025-04-07 21:11:30 +0200

    Update logintestcase

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,6 +1,8 @@
+import pytest
 from selenium import webdriver
 from pageObjects.loginPage import LoginPage
 from pageObjects.homePage import HomePage
+
 
 class Test_001_login:
     base_URL = "https://awesomeqa.com/ui"
@@ -10,8 +12,8 @@ class Test_001_login:
     invalid_password = "admin123"
     title = "My Account"
 
-    def test_login_with_valid_credentials(self):
-        driver = webdriver.Chrome()
+    def test_login_with_valid_credentials(self, setUp):
+        driver = setUp
         driver.get(self.base_URL)
         driver.maximize_window()
         home_page = HomePage(driver)
@@ -24,12 +26,12 @@ class Test_001_login:
         current_title = driver.title
         if current_title == self.title:
             assert True
+            driver.close()
         else:
             assert False
 
-
-    def test_login_with_invalid_credentials(self):
-        driver = webdriver.Chrome()
+    def test_login_with_invalid_credentials(self, setUp):
+        driver = setUp
         driver.get(self.base_URL)
         driver.maximize_window()
         home_page = HomePage(driver)
@@ -41,5 +43,40 @@ class Test_001_login:
         login_page.click_login_button()
         if login_page.check_error_message():
             assert True
+            driver.close()
+        else:
+            assert False
+
+    def test_login_with_invalid_email_(self, setUp):
+        driver = setUp
+        driver.get(self.base_URL)
+        driver.maximize_window()
+        home_page = HomePage(driver)
+        home_page.click_my_account()
+        home_page.click_my_account_login()
+        login_page = LoginPage(driver)
+        login_page.input_eMail(self.invalid_email)
+        login_page.input_password(self.password)
+        login_page.click_login_button()
+        if login_page.check_error_message():
+            assert True
+            driver.close()
+        else:
+            assert False
+
+    def test_login_with_invalid_password(self, setUp):
+        driver = setUp
+        driver.get(self.base_URL)
+        driver.maximize_window()
+        home_page = HomePage(driver)
+        home_page.click_my_account()
+        home_page.click_my_account_login()
+        login_page = LoginPage(driver)
+        login_page.input_eMail(self.email)
+        login_page.input_password(self.invalid_password)
+        login_page.click_login_button()
+        if login_page.check_error_message():
+            assert True
+            driver.close()
         else:
             assert False
